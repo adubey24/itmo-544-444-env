3ef09cebfb6fa44f6b42411c4a32f38aff669d99
controller <adubey4@hawk.iit.edu>
2015-11-03 18:45:03 -0600

    Added code to pull other github account

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2 git
+
+git clone https://github.com/adubey24/itmo-544-444-fall2015.git
+
+mv ./itmo-544-444-fall2015/images /var/www/html/images
+mv ./itmo-544-444-fall2015/index.html /var/www/html
 
 echo "Hello!" > /tmp/hello.txt
 
